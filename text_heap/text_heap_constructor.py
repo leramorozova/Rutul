f2b812870adb01f6362fdd74bbfccb7efde28a8b
valeria <leramorozova@github.com>
2018-12-29 12:43:04 +0200

    text_heap

--- text_heap/text_heap_constructor.py
+++ text_heap/text_heap_constructor.py
@@ -1,6 +1,15 @@
 from praatio import tgio
 from functools import wraps
+import re
 import os
+
+"""
+В поле лосс не должно быть пустых мест.
+Границы крайних форм должны совпадать с границами предложений.
+
+"""
+
+LEXEMES = 0
 
 
 class ParseTextGrid:
@@ -43,76 +52,100 @@ class MakeGlossLine(ParseTextGrid):
                         j += 1
         return idx_list
 
+    @staticmethod
+    def remove_tags(morph_line, gloss_line):
+        morph_line = re.sub("<b>", "&lt;b&gt;", morph_line)
+        morph_line = re.sub("<w>", "&lt;w&gt;", morph_line)
+        morph_line = re.sub("<r>", "&lt;r&gt;", morph_line)
+        morph_line = re.sub("<d>", "&lt;d&gt;", morph_line)
+        morph_line = re.sub("<t>", "&lt;t&gt;", morph_line)
+        gloss_line = re.sub("<HPL>", "&lt;HPL&gt;", gloss_line)
+        gloss_line = re.sub("<APL>", "&lt;APL&gt;", gloss_line)
+        return morph_line, gloss_line
+
     def make_line(self):
+        lines = []
         morph = self.morph()
         gloss = self.gloss()
         idxs = self.sent_start_end(morph)
+        max = 9
         for i, sentence in enumerate(idxs):
-            morph_line = ''
-            gloss_line = ''
-            len_gloss_line = 0
-            len_morph_line = 0
-            morph_line += str(i + 1) + ') '
-            gloss_line += ' ' * (2 + len(str(i + 1)))
-            len_gloss_line += 2 + len(str(i + 1))
-            len_morph_line += 2 + len(str(i + 1))
+            morph_line = "<tr>"
+            gloss_line = "<tr>"
+            morph_line += "<td>(" + str(i + 1) + ") </td>"
+            gloss_line += "<td>" + ' ' * len(str(i + 2)) + "</td>"
             start_idx = sentence[0]
             end_idx = sentence[1]
-            while start_idx != end_idx + 1:
-                morph_line += morph[start_idx][2]
-                gloss_line += gloss[start_idx][2]
-                len_morph_line += len(morph[start_idx][2])
-                len_gloss_line += len(gloss[start_idx][2])
-                if len_morph_line > len_gloss_line:
-                    morph_line += '\t'
-                    if len(morph[start_idx][2]) % 4 != 0:
-                        flag = 4 - len(morph[start_idx][2]) % 4
-                    else:
-                        flag = 4
-                    len_morph_line += flag
-                    while len_morph_line > len_gloss_line:
-                        gloss_line += '\t'
-                        if gloss_line[-2] != '\t':
-                            len_gloss_line += (4 - len_gloss_line % 4)
-                        else:
-                            len_gloss_line += 4
-                else:
-                    gloss_line += '\t'
-                    if len(gloss[start_idx][2]) % 4 != 0:
-                        flag = 4 - len(gloss[start_idx][2]) % 4
-                    else:
-                        flag = 4
-                    len_gloss_line += flag
-                    while len_gloss_line > len_morph_line:
-                        morph_line += '\t'
-                        if morph_line[-2] != '\t':
-                            len_morph_line += (4 - len_morph_line % 4)
-                        else:
-                            len_morph_line += 4
+            while max > 0 and start_idx != end_idx + 1:
+                global LEXEMES
+                LEXEMES += 1
+                morph_line += "<td>" + morph[start_idx][2] + "   </td>"
+                gloss_line += "<td>" + gloss[start_idx][2] + "   </td>"
                 start_idx += 1
-            yield (morph_line, gloss_line)
+                max -= 1
+                if max == 0:
+                    morph_line, gloss_line = self.remove_tags(morph_line, gloss_line)
+                    morph_line += "</tr>"
+                    gloss_line += "</tr>"
+                    lines.append((morph_line, gloss_line))
+                    morph_line = "<tr>"
+                    gloss_line = "<tr>"
+                    max = 9
+            morph_line, gloss_line = self.remove_tags(morph_line, gloss_line)
+            morph_line += "</tr>"
+            gloss_line += "</tr>"
+            lines.append((morph_line, gloss_line))
+            max = 9
+        return lines
 
     def translation_block(self):
-        pass
+        block = ''
+        morph = self.morph()
+        translation = self.translation()
+        idxs = self.sent_start_end(morph)
+        i = 1
+        for el in idxs:
+            start = morph[el[0]][0]
+            for sent in translation:
+                if sent[0] == start:
+                    block += '(' + str(i) + ') '
+                    block += sent[2].capitalize()
+                    if sent[2][-1] in ',.?!/:':
+                        pass
+                    else:
+                        block += '.'
+                    block += '<br> '
+                    i += 1
+        return block
+
+"""
+Starting html construction
+
+"""
 
 
 def write_html_header(func):
     @wraps(func)
-    def wrapper():
+    def wrapper(filename):
         header = """ <!DOCTYPE html>
         <html>
         <head>
         <meta charset="utf-8" />
         <title>Свалка рутульских текстов</title>
         <style type="text/css">
-        div {
-            width: 100%; /* Ширина */
-            background: #fc0; /* Цвет фона */
-            padding: 20px; /* Поля */
-            -moz-box-sizing: border-box; /* Для Firefox */
-            -webkit-box-sizing: border-box; /* Для Safari и Chrome */
-            box-sizing: border-box; /* Для IE и Opera */
+        body {
+            padding: 40px;
+            line-height: 1.5;
+        }
+        .example {
+            padding: 1px; /* Поля */
+            white-space: pre;
             }
+        p {
+            width: 80%;
+            border: 1px solid black;
+            padding: 10px;
+        }
         </style>
         </head>
         <body>
@@ -120,8 +153,9 @@ def write_html_header(func):
         with open("text_heap.html", 'a', encoding="UTF-8") as html:
             html.write(header)
         html.close()
-        func()
-        bottom = """</body>
+        func(filename)
+        bottom = """
+        </body>
         </html>
         """
         with open("text_heap.html", 'a', encoding="UTF-8") as html:
@@ -131,14 +165,31 @@ def write_html_header(func):
 
 
 @write_html_header
-def fuck():
-    header = "FUUUUUUUUCK"
+def put_texts(filename):
     with open("text_heap.html", 'a', encoding="UTF-8") as html:
-        html.write(header)
-    html.close()
+        html.write("<center><b><caption><font size=\"5\"> \n" + filename.split('.')[0])
+        html.write("\n</font></center></b></caption><br>")
+        html.close()
+    print(filename)
+    parser = MakeGlossLine(os.path.join("srcs", filename))
+    for el in parser.make_line():
+        with open("text_heap.html", 'a', encoding="UTF-8") as html:
+            html.write("<table class=\"example\">\n" + el[0] + '\n' + el[1] + '\n' + "</table>")
+        html.close()
+    with open("text_heap.html", 'a', encoding="UTF-8") as html:
+        html.write("<p>\n" + parser.translation_block() + "</p><br><br>")
+        html.close()
 
 
+def main():
+    try:
+        os.remove("text_heap.html")
+    except Exception:
+        pass
+    for tg in os.listdir("srcs"):
+        put_texts(tg)
+    print("\nКолчество лексем в копрусе: " + str(LEXEMES))
 
 
-parser = MakeGlossLine(os.path.join("srcs", "180722_0090.TextGrid"))
-parser.translation_block()
+if __name__ == "__main__":
+    main()
